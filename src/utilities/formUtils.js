addf785b9263065c211557107727cf9482796216
Cornelius <bcornelius@iseatz.com>
2021-11-04 17:23:17 -0500

    adds html call frame

--- src/utilities/formUtils.js
+++ src/utilities/formUtils.js
@@ -1,5 +1,16 @@
 const getEmailEntries = () => Array.from(document.getElementsByClassName('input-row')).map(e => e.value);
 
+const submitValues = values => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('submitting values', () => {
+        console.log(xhr.responseText);
+    });
+    xhr.open('POST', process.env.REACT_APP_APIURL);
+    xhr.setRequestHeader('X-Api-Key', process.env.REACT_APP_APIKEY);
+    xhr.send(JSON.stringify(values));
+};
+
 export const submitFormEntries = ({surveyName, url}) => {
-    console.log(surveyName, url, getEmailEntries())
+    console.log(surveyName, url, getEmailEntries());
+    // submitValues({submitValues, url, emails: getEmailEntries()});
 }
